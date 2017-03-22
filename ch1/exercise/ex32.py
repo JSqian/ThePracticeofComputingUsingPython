23867a483764d9dd778b241d78087efa4db63b23
qjs <2435587159@qq.com>
2017-03-22 17:02:24 +0800

    complete exercise 32

---
+++ ch1/exercise/ex32.py
@@ -0,0 +1,12 @@
+completions = int(raw_input("Enter the number of completions:"))
+attempts = int(raw_input("Enter the number of attempts:"))
+yards = int(raw_input("Enter the yards:"))
+touchDowns = int(raw_input("Enter the number of touchdowns:"))
+interceptions = int(raw_input("Enter the number of interceptions:"))
+
+C = (((completions)*1.0/attempts)*100 - 30) / 20
+Y = (yards*1.0/attempts -3)/4
+T = (touchDowns*1.0/attempts)*20
+I = 2.375 - (interceptions*1.0/attempts)*25
+rate = (C+Y+T+I)/6*100
+print rate
